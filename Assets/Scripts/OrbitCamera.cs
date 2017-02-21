4a596b4a17047d9d302274d8a6fb603004e35e45
caizicong <dorightlife@163.com>
2017-02-21 12:12:53 +0800

    实现第三人称 3D 游戏中玩家的移动和动画 * 将角色模型导入到场景 * 实现摄像机控制，以观察角色 * 编写脚本，让玩家能够在地面上跑 * 添加能够跳跃、移动的脚本 * 基于移动来播放模型的动画

---
+++ Assets/Scripts/OrbitCamera.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class OrbitCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    public float rotSpeed = 1.5f;
+
+    private float _rotY;
+    private Vector3 _offset;
+
+    void Start()
+    {
+        _rotY = this.transform.eulerAngles.y;
+        _offset = target.position - this.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        float horInput = Input.GetAxis("Horizontal");
+
+        if (0 != horInput)
+        {
+            _rotY = _rotY + horInput * rotSpeed;
+        }
+        else
+        {
+            _rotY = _rotY + Input.GetAxis("Mouse X") * rotSpeed * 3;
+        }
+
+        Quaternion rotation = Quaternion.Euler(0, _rotY, 0);
+        this.transform.position = target.position - (rotation * _offset);
+        this.transform.LookAt(target);
+    }
+}
---
+++ Assets/Scripts/RelativeMovement.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class RelativeMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    public float rotSpeed = 15.0f;
+    public float moveSpeed = 6.0f;
+    public float jumpSpeed = 15.0f;
+    public float gravity = -9.8f;
+    public float terminalVelocity = -10.0f;
+    public float minFall = -1.5f;
+
+    private float _vertSpeed;
+    private CharacterController _characterController;
+
+    private ControllerColliderHit _contact;
+
+    private Animator _animator;
+
+    void Start()
+    {
+        _characterController = this.GetComponent<CharacterController>();
+
+        _vertSpeed = minFall;
+
+        _animator = this.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Vector3 movement = Vector3.zero;
+
+        float horInput = Input.GetAxis("Horizontal");
+        float vertInput = Input.GetAxis("Vertical");
+
+        if (0 != horInput || 0 != vertInput)
+        {
+            movement.x = horInput * moveSpeed;
+            movement.z = vertInput * moveSpeed;
+            movement = Vector3.ClampMagnitude(movement, moveSpeed);
+
+            Quaternion tmp = target.rotation;
+            target.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
+            movement = target.TransformDirection(movement);
+            target.rotation = tmp;
+
+            Quaternion direction = Quaternion.LookRotation(movement);
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, direction, rotSpeed * Time.deltaTime);
+        }
+
+        _animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        bool hitGround = false;
+        RaycastHit hit;
+        bool isHit = Physics.Raycast(this.transform.position, Vector3.down, out hit);
+
+        if (_vertSpeed < 0 && isHit)
+        {
+            float check = (_characterController.height + _characterController.radius) / 1.9f;
+            hitGround = (hit.distance <= check);
+        }
+
+        if (hitGround)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                _vertSpeed = jumpSpeed;
+            }
+            else
+            {
+                _vertSpeed = -0.1f;
+                _animator.SetBool("Jumping", false);
+            }
+        }
+        else
+        {
+            _vertSpeed = _vertSpeed + gravity * 5 * Time.deltaTime;
+            if (_vertSpeed < terminalVelocity)
+            {
+                _vertSpeed = terminalVelocity;
+            }
+
+            if (null != _contact)
+            {
+                _animator.SetBool("Jumping", true);
+            }
+
+            if (_characterController.isGrounded)
+            {
+                if (Vector3.Dot(movement, _contact.normal) < 0)
+                {
+                    movement = _contact.normal * moveSpeed;
+                }
+                else
+                {
+                    movement = movement + _contact.normal * moveSpeed;
+                }
+            }
+        }
+        movement.y = _vertSpeed;
+
+        movement = movement * Time.deltaTime;
+        _characterController.Move(movement);
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        _contact = hit;
+    }
+}
